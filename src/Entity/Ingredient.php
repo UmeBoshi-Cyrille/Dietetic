8fca42718b7aa6be5214e0900b713dd854713df0
Cyrille Umehara <Cyrilleh@live.fr>
2022-05-10 11:08:30 +0200

    Add Recette && ingredient entity && constraints

---
+++ src/Entity/Ingredient.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\IngredientRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: IngredientRepository::class)]
+#[UniqueEntity('name')]
+class Ingredient
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 55)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 55)]
+    private string $name;
+
+    #[ORM\Column(type: 'text')]
+    #[Assert\NotNull()]
+    private $description;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull()]
+    private ?DateTimeImmutable $publishedAt;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    private $isPublished;
+
+    public function __construct()
+    {
+        $this->publishedAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeImmutable $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(?bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Recette.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecetteRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: RecetteRepository::class)]
+#[UniqueEntity('name')]
+class Recette
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 255)]
+    private string $name;
+
+    #[ORM\Column(type: 'text')]
+    #[Assert\NotNull()]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 75, nullable: true)]
+    #[Assert\NotBlank()]
+    #[Assert\Length(min: 2, max: 75)]
+    private string $preparationTime;
+
+    #[ORM\Column(type: 'string', length: 75, nullable: true)]
+    #[Assert\Length(min: 2, max: 75)]
+    private string $restTime;
+
+    #[ORM\Column(type: 'string', length: 75, nullable: true)]
+    #[Assert\Length(min: 2, max: 75)]
+    private string $cookingTime;
+
+    #[ORM\Column(type: 'text')]
+    #[Assert\NotNull()]
+    private $step;
+
+    #[ORM\Column(type: 'datetime_immutable')]
+    #[Assert\NotNull()]
+    private ?DateTimeImmutable $publishedAt;
+
+    #[ORM\Column(type: 'boolean', nullable: true)]
+    private $isPublished;
+
+    public function __construct()
+    {
+        $this->publishedAt = new DateTimeImmutable();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPreparationTime(): ?string
+    {
+        return $this->preparationTime;
+    }
+
+    public function setPreparationTime(?string $preparationTime): self
+    {
+        $this->preparationTime = $preparationTime;
+
+        return $this;
+    }
+
+    public function getRestTime(): ?string
+    {
+        return $this->restTime;
+    }
+
+    public function setRestTime(?string $restTime): self
+    {
+        $this->restTime = $restTime;
+
+        return $this;
+    }
+
+    public function getCookingTime(): ?string
+    {
+        return $this->cookingTime;
+    }
+
+    public function setCookingTime(?string $cookingTime): self
+    {
+        $this->cookingTime = $cookingTime;
+
+        return $this;
+    }
+
+    public function getStep(): ?string
+    {
+        return $this->step;
+    }
+
+    public function setStep(string $step): self
+    {
+        $this->step = $step;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeImmutable $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(?bool $isPublished): self
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+}
